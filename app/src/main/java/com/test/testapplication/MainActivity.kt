3d0beb8e2a63e81d7edbf6297384be53d67be913
Elizaveta Nevostrueva <elizaveta.nevostrueva@umbrella-web.com>
2018-11-11 15:18:41 +0300

    Implemented  gradle settings and UI

--- app/src/main/java/com/test/testapplication/MainActivity.kt
+++ app/src/main/java/com/test/testapplication/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.test.testapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buildVersionName.text = BuildConfig.BUILD_VARIANT_NAME
     }
 }
